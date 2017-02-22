9b58d15e3e24ffe8e5a5aa12386079fe67d0726d
Balázs Dukai <balazs.dukai@gmail.com>
2017-02-22 16:19:15 +0100

    geometry_primitives: init

---
+++ City3DReadr/geometry_primitives.py
@@ -0,0 +1,48 @@
+"""
+Created on 22 Feb 2017
+
+@author: Balázs Dukai
+"""
+
+class Polygon(object):
+    """
+    classdocs
+    """
+
+
+    def __init__(self, params):
+        """
+        Constructor
+        """
+        # TODO: generate a python object from an input (WKT / vertex list), has to handle 3D
+        print("Method not implemented")
+    
+    def count_vertices(self):
+        """
+        Return: the number of vertices in the polygon
+        """
+        
+        print("Method not implemented")
+        
+    def area(self):
+        """
+        Return: the area of the polygon
+        """
+        
+        print("Method not implemented")
+    
+    def length_edges(self):
+        """
+        Return: list of edge lengths in units of the CRS
+        """
+        
+        print("Method not implemented")
+        
+    def NSCP(self):
+        """
+        Return: number of shape characterising points
+        """ 
+        
+        print("Method not implemented")
+        
+        
